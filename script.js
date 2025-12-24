284431e06f1a04ad66c86cc16c970bc7db8b8508
vivekdhapa <vivekdhapa24@gmail.com>
2025-12-25 02:50:38 +0530

    final commit

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const todoInput=document.getElementById('todo-input');
+document.addEventListener('DOMContentLoaded',()=>{
+    const todoInput=document.getElementById('todo-input');
 const addTaskButton=document.getElementById('add-task-btn');
 const todoList=document.getElementById('todo-list');
 
-let tasks=[]
+let tasks=JSON.parse(localStorage.getItem('tasks') )|| [];
+
+tasks.forEach(task => renderTasks(task ));
 
 addTaskButton.addEventListener('click', ()=>{
     const taskText=todoInput.value.trim()//removes all the extra spaces
@@ -11,9 +14,47 @@ addTaskButton.addEventListener('click', ()=>{
     const newTask={
         id: Date.now(),
         text: taskText,
-        isCompleted:false
+        completed:false
     }
-    tasks.push(newTask)
+    tasks.push(newTask);
+    saveTasks();
+    renderTasks(newTask);
     todoInput.value=""; //clear input
-    console.log(tasks)
+    console.log(tasks);
 });
+
+
+
+function renderTasks(task){
+        const li=document.createElement("li");
+        li.setAttribute("data-id",task.id);
+        if (task.completed) {
+            li.classList.add("completed")
+        }
+        li.innerHTML=`
+        <span>${task.text}</span>
+        <button>delete</button>
+        `;
+        li.addEventListener('click', (e)=>{
+            if(e.target.tagName==='BUTTON') return;
+            task.completed=!task.completed
+            li.classList.toggle('completed')
+            saveTasks();
+        });
+                
+        li.querySelector('button').addEventListener('click',                      (e)=>{
+            e.stopPropagation()
+            tasks=tasks.filter(t => t.id!==task.id)
+            li.remove();
+            saveTasks();
+        })
+
+
+
+        todoList.appendChild(li);
+}
+
+function saveTasks(){
+    localStorage.setItem('tasks',JSON.stringify(tasks))
+}
+})
